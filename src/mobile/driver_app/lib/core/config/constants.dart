afe4146da0c4d2846c00f1fbe7a18f2622a6a191
arvindverma63 <info@backendcodersindia.com>
2026-08-07 13:42:58 +0530

    Update webview base URL to point to Vercel deployment

--- src/mobile/driver_app/lib/core/config/constants.dart
+++ src/mobile/driver_app/lib/core/config/constants.dart
@@ -4,8 +4,8 @@ class AppConfig {
 
   // 10.0.2.2 points to localhost of the host machine from the Android emulator.
   // Use http://localhost:4200 if compiling for physical device on the same local server.
-  static const String devBaseUrl = 'http://10.0.2.2:4200';
-  static const String prodBaseUrl = 'https://driver-portal.redtaxi.co.uk';
+  static const String devBaseUrl = 'https://red-taxis-webview-yplz.vercel.app';
+  static const String prodBaseUrl = 'https://red-taxis-webview-yplz.vercel.app';
 
   static String get webviewBaseUrl => useDevUrl ? devBaseUrl : prodBaseUrl;
 
